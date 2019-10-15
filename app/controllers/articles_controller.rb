0f88d831d23f8c3150cc908492a0df01467d05d1
carmenluo <Sdluojm@gmail.com>
2019-10-15 13:01:25 -0400

    artcile update

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,14 +1,29 @@
 class ArticlesController < ApplicationController
-  def new
-
-  end
   def index
     @articles = Article.all
   end
+
+  def new
+    @article = Article.new
+  end
+  def edit
+    @article = Article.find(params[:id])
+  end
   def create
     @article = Article.new(article_params)
-    @article.save
+    if @article.save
     redirect_to @article
+    else
+      render 'new'
+    end
+  end
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render 'edit'
+    end
   end
   def show
     @article = Article.find(params[:id])
